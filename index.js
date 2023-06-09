e297797f0e44d2e6c6c0a9223895375512bd204f
Filip Wojtowicz <wojtowicz2222@gmail.com>
2023-06-09 12:03:57 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// http://api.nbp.pl/api/exchangerates/rates/a/usd/
+const converterForm = document.getElementById("converter-form");
+const currencyValue = document.getElementById("currency-value");
+const currencySelect = document.getElementById("currency-select");
+const currencyButton = document.getElementById("submit-button");
+const currencyParagraph = document.getElementById("result-paragraph");
+
+function getCurrency(event) {
+  event.preventDefault();
+  fetch(`http://api.nbp.pl/api/exchangerates/rates/a/${currencySelect.value}/`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.rates?.[0]?.mid) {
+        currencyParagraph.innerHTML = `${(
+          Number(currencyValue.value) * data.rates[0].mid
+        ).toFixed(2)}PLN`;
+      } else {
+        alert("something goes wrong :( Come back later pliz");
+      }
+    })
+    .catch(() => {
+      alert("something goes wrong :( Come back later pliz");
+    });
+}
+
+converterForm.addEventListener("submit", getCurrency);
